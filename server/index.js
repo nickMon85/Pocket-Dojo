33c8464729ef292cbad5539a787ef1372e86dd93
Ramon Bolds <ramonbolds98@gmail.com>
2020-12-07 21:05:31 -0600

    tested backend in terminal connected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,24 +1,5 @@
-// import express from 'express';
-// import bodyParser from 'body-parser';
-// import mongoose from 'mongoose';
-// import cors from 'cors';
-
-// const app = express();
-// app.use(bodyParser.json({limit:"30mb", extended: true}));
-// app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
-// app.use(cors());
-
-
-// const PORT = process.env.PORT|| 5000;
-
-// mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
-//     .then(()=> app.listen(PORT,()=>console.log('Server running on port:${PORT}')))
-//     .catch((error)=>console.log(error.message))
-
-// mongoose.set('useFindAndModify',false);
-
 const { connect } = require('mongoose');
-const connectDB = require('./startup/db');
+const connectDB = require('./startup/DB');
 const express = require('express');
 const app = express();
 
