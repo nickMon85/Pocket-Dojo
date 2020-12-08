c5efc95e9377899de76b1e096c5f8a531f2b97a1
Ramon Bolds <ramonbolds98@gmail.com>
2020-12-07 20:27:51 -0600

    "fixing mongo connection in Terminal

--- server/index.js
+++ server/index.js
@@ -1,4 +1,31 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+// import express from 'express';
+// import bodyParser from 'body-parser';
+// import mongoose from 'mongoose';
+// import cors from 'cors';
+
+// const app = express();
+// app.use(bodyParser.json({limit:"30mb", extended: true}));
+// app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
+// app.use(cors());
+
+
+// const PORT = process.env.PORT|| 5000;
+
+// mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+//     .then(()=> app.listen(PORT,()=>console.log('Server running on port:${PORT}')))
+//     .catch((error)=>console.log(error.message))
+
+// mongoose.set('useFindAndModify',false);
+
+const { connect } = require('mongoose');
+const connectDB = require('./startup/db');
+const express = require('express');
+const app = express();
+
+connectDB();
+app.use(express.json());
+
+const port = process.env.PORT||5000;
+app.listen(port, () => {
+    console.log(`Server started on port:${port}`)
+});
