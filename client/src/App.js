ab341c565fcc9e2782025b54cc1c681eff4ac743
Ramon Bolds <ramonbolds98@gmail.com>
2020-12-09 05:15:54 -0600

    Remade my client side folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './App.css';
 
-const App = () =>{
-    return(
-        <div>
-            <h1>App</h1>
-        </div>
-    );
+const App =()=>{
+  return(
+    <div>
+      <h1>App</h1>
+    </div>
+  );
 }
+
 export default App;
--- server/index.js
+++ server/index.js
@@ -31,8 +31,8 @@ const app = express();
 //Test by using localhost:5000/posts
 app.use('/posts',postRoutes);
 
-app.use(express.json({limit:"30mb", extended: true}));
-app.use(express.urlencoded({limit:"30mb", extended: true}));
+app.use(bodyParser.json({limit:"30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
 app.use(cors());
 
 const CONNECTION_URL = "mongodb+srv://Monbolds:semperfi@pocketdojo.bqqpg.mongodb.net/PocketDojo?retryWrites=true&w=majority"
